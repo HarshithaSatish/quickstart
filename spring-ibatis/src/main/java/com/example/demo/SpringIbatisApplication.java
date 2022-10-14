b7bdac06554a07a1ed6aed32cda4664171db2068
hsssshar <hsssshar@WPG02VBMD>
2022-10-14 14:44:35 +0530

    spring-ibatis example added

---
+++ spring-ibatis/src/main/java/com/example/demo/SpringIbatisApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.demo.model.Customer;
+import com.example.demo.services.CustomerService;
+
+@SpringBootApplication
+public class SpringIbatisApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx=SpringApplication.run(SpringIbatisApplication.class, args);
+		
+		Customer obj=ctx.getBean(Customer.class);
+			obj.setCustomerId(222);
+			obj.setCustomerName("Meenakshi");
+			obj.setEmail("meena@abc.com");
+			
+			CustomerService service=ctx.getBean(CustomerService.class);
+			System.out.println("Is Added:="+ service.save(obj));
+		service.findAll().forEach(System.out::println);
+		
+		ctx.close();
+	}
+
+}
